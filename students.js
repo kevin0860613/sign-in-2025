1a34587b51b1639e1ba68efba98b9631b6a6d75f
kevin0860613 <b10415041@gmail.com>
2025-09-18 15:57:04 +0800

    students.js

---
+++ students.js
@@ -0,0 +1,21 @@
+const VALID_STUDENTS = [
+  "quartararorossi2046@gmail.com",
+  "a28616319@yahoo.com.tw",
+  "a82452898@gmail.com",
+  "rex940723@gmail.com",
+  "nmeknne23@gmail.com",
+  "kyrayehsoul@gmail.com",
+  "annie20021117@gmail.com",
+  "1070903@gmail.com",
+  "clara3333333@gmail.com",
+  "cchangray@gmail.com"
+];
+
+const EXEMPT_STUDENTS = [
+  "b10415041@gmail.com", // 凱文
+  "cz0806.phone@gmail.com" // 小逸
+];
+
+const TEST_STUDENTS = [
+  "2024tfciaclass@gmail.com" // 星引力
+];
