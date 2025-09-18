8a73f98cb95cb3772a0fbf2d36f5d193406058f7
kevin0860613 <b10415041@gmail.com>
2025-09-18 20:53:34 +0800

    Update students.js

--- students.js
+++ students.js
@@ -1,6 +1,6 @@
 // 所有學生名單（email → name）
 const STUDENTS = {
-  "quartararorossi2046@gmail.com": "蕭文彥",
+  "xioawenyan78@gmail.com": "蕭文彥",
   "a28616319@yahoo.com.tw": "吳秉翰",
   "a82452898@gmail.com": "林可馨",
   "rex940723@gmail.com": "魏晨名",
